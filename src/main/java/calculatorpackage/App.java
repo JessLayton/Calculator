00cdd29da9e231ce2536e14d00e8c0c7ed74d885
JessLayton <jessylayton@yahoo.co.uk>
2019-12-03 11:41:42 +0000

    First commit

---
+++ src/main/java/calculatorpackage/App.java
@@ -0,0 +1,11 @@
+package calculatorpackage;
+
+public class App {
+	
+	public static void main(String[] args) {
+		calculateValue();		
+		
+	}
+	
+
+}
---
+++ src/main/java/calculatorpackage/Calculator.java
@@ -0,0 +1,7 @@
+package calculatorpackage;
+
+public class Calculator {
+
+	
+
+}
